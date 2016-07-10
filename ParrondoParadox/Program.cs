44933c57c76c6d97ec40a0bf0e92f455982d9467
Logan Apple <loganapple@hotmail.com>
2016-07-10 07:10:20 -0400

    First Commit
    The initial version of Parrondo's Paradox, written in C#. Note: in
    order to aptly test the paradox, the coinToss variable must be set to
    rand.Next(1, 1) and rand.Next(2, 2) independently for running to show
    the probability of victory of the first and second set, respectively.
    rand.Next(1, 3) is the randomization between these two sets.

---
+++ ParrondoParadox/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParrondoParadox
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int position = 0, gamesWon = 0, gamesLost = 0;
+			List<int> orangeLeft = new List<int> { 2, 4, 12 }, orangeRight = new List<int> { 11 }, yellowLeft = new List<int> { 2, 3, 12 }, yellowRight = new List<int> { 7, 11 };
+			List<int> secondOrangeLeft = new List<int> { 2, 3, 12 }, secondOrangeRight = new List<int> { 7, 11 }, secondYellowLeft = new List<int> { 2, 4, 12 }, secondYellowRight = new List<int> { 11 };
+			double probability = 0;
+			Random rand = new Random();
+
+			while (position != -2 || position != 2)
+			{
+				int coinToss = rand.Next(1, 3);
+				int diceRoll = rand.Next(1, 13);
+
+				if (coinToss == 1)
+				{
+					if (position == 0)
+					{
+						if (orangeLeft.Contains(diceRoll))
+							position--;
+						else if (orangeRight.Contains(diceRoll))
+							position++;
+					}
+					else if (position == -1 || position == 1)
+					{
+						if (yellowLeft.Contains(diceRoll))
+							position--;
+						else if (yellowRight.Contains(diceRoll))
+							position++;
+					}
+				}
+				else {
+					if (position == 0)
+					{
+						if (secondOrangeLeft.Contains(diceRoll))
+							position--;
+						else if (secondOrangeRight.Contains(diceRoll))
+							position++;
+					}
+					else if (position == -1 || position == 1)
+					{
+						if (secondYellowLeft.Contains(diceRoll))
+							position--;
+						else if (secondYellowRight.Contains(diceRoll))
+							position++;
+					}
+				}
+
+				if (position == -2)
+				{
+					gamesLost++;
+					position = 0;
+					if (gamesLost != 0) probability = (double)gamesWon / gamesLost;
+				}
+				else if (position == 2)
+				{
+					gamesWon++;
+					position = 0;
+					if(gamesLost != 0) probability = (double)gamesWon / gamesLost;
+				}
+
+				if (gamesWon + gamesLost == 1000000)
+					break;
+			}
+
+			Console.WriteLine("Number of games played: " + (gamesWon + gamesLost) + "\nNumber of games lost: " + gamesLost + "\nNumber of games won: " + gamesWon + "\n\nApproximate probability of victory: " + probability);
+			Console.Read();
+		}
+	}
+}
